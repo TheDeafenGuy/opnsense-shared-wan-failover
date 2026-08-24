c9c18669b34afd96018c88403a5a6f3b1ddcfcb8
TheDeafenGuy <118931638+TheDeafenGuy@users.noreply.github.com>
2026-08-24 09:22:34 -0500

    update script for KEA dhcp

--- wan-carp-monitor.sh
+++ wan-carp-monitor.sh
@@ -1,33 +1,23 @@
 #!/bin/sh
-# WAN CARP monitor for backup firewall
-# Only runs linkup start/stop when CARP state changes
-
-IF="[replace with your WAN interface]"
-CARP_IF="[replace with interface that has CARP VIPS]"   # interface carrying VIP
+IF="vtnet1"
+CARP_IF="vtnet0_vlan352"
 LOGTAG="carp-wan-monitor"
 PREV_STATE=""
-
 while true; do
-    # Get current CARP state
-    if ifconfig $CARP_IF | grep -q 'MASTER'; then
-        CUR_STATE="MASTER"
-    else
-        CUR_STATE="BACKUP"
-    fi
-
-    # Only act if state has changed
-    if [ "$CUR_STATE" != "$PREV_STATE" ]; then
-        if [ "$CUR_STATE" = "MASTER" ]; then
-            logger -t $LOGTAG "CARP MASTER: enabling $IF"
-            /usr/local/sbin/configctl interface linkup start $IF
-            /usr/local/sbin/configctl dhcpd start            
-        else
-            logger -t $LOGTAG "CARP BACKUP: disabling $IF"
-            /usr/local/sbin/configctl interface linkup stop $IF
-            /usr/local/sbin/configctl dhcpd stop            
-        fi
-        PREV_STATE="$CUR_STATE"
-    fi
-
-    sleep 5
+if ifconfig $CARP_IF | grep -q 'MASTER'; then
+CUR_STATE="MASTER"
+else
+CUR_STATE="BACKUP"
+fi
+if [ "$CUR_STATE" != "$PREV_STATE" ]; then
+if [ "$CUR_STATE" = "MASTER" ]; then
+logger -t $LOGTAG "CARP MASTER: enabling $IF"
+/usr/local/sbin/configctl interface linkup start $IF
+else
+logger -t $LOGTAG "CARP BACKUP: disabling $IF"
+/usr/local/sbin/configctl interface linkup stop $IF
+fi
+PREV_STATE="$CUR_STATE"
+fi
+sleep 5
 done
